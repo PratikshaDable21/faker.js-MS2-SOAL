9b3eba5457b8b9341cebcf6b621c605835fd8cf6
Tushar Dighe <dighetushar654@gmail.com>
2021-03-08 14:45:13 +0530

    update hotel file

--- test/hotel.unit.js
+++ test/hotel.unit.js
@@ -2,36 +2,16 @@ if (typeof module !== 'undefined') {
     var assert = require('assert');
     var sinon = require('sinon');
     var faker = require('../index');
-  }
-  
-  describe("hotel.js", function () {
-    describe("hotel()", function () {
-        it("returns a random hotel", function () {
-            sinon.stub(faker.hotel, 'hotel').returns('Marriott');
-            var hotel = faker.hotel.();
-  
-            assert.equal(vehicle, 'Marriott');
-            faker.hotel.restore();
-        });
-    });
-  
-    describe("address()", function () {
-        it("returns random address", function () {
-            sinon.stub(faker.hotel, 'address').returns('Mumbai_India');
-            var address = faker.hotel.address();
-  
-            assert.equal(address, 'Mumbai_India');
-            faker.hotel.address.restore();
-        });
-    });
-  
-    describe("typeofstay()", function () {
-      it("returns random hotel typeofstay", function () {
-        sinon.stub(faker.hotel, 'typeofstay').returns('Business');
-            var typeofstay = faker.hotel.typeofstay();
-  
-            assert.equal(typeofstay, 'Business');
-            faker.hotel.typeofstay.restore();
+}
+
+describe("hotel.js", function () {
+    describe("type()", function () {
+        it("returns a type", function () {
+            sinon.stub(faker.hotel, 'type').returns('Star hotel');
+            var type = faker.hotel.type();
+
+            assert.strictEqual(type, 'Star Hotel');
+            faker.hotel.type.restore();
         });
     });
 });
